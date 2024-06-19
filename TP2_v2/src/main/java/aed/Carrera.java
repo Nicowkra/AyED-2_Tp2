5a6315f663c010e73c23ba7c2ddacdee8b1e0a9d
Lukksa22 <la1247890@gmail.com>
2024-06-19 17:35:41 -0300

    agregados metodos faltantes

--- TP2_v2/src/main/java/aed/Carrera.java
+++ TP2_v2/src/main/java/aed/Carrera.java
@@ -1,17 +1,23 @@
 package aed;
 
 public class Carrera {
-    private Trie<Materia> _materias;
+    private DictTrie<Materia> _materias;
 
 public Carrera(){
-    this. _materias = new Trie<Materia>();
+    this. _materias = new DictTrie<Materia>();
 }
 
 public void crearMateria(String nombreMateria, Materia _materia){
     this._materias.agregar(nombreMateria, _materia);
 }
 public Materia obtenerMateria(String nombreMateria){
-    return this._materias.obtener(nombreMateria);
+    return (Materia) this._materias.obtener(nombreMateria);
+}
+public void cerrarMateria(String nombreMateria){
+    this._materias.eliminar(nombreMateria);
+}
+public String[] materiasEnCarreras() {
+    return _materias.palabras();
 }
 
 }
